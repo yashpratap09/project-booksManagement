67cf57677d293395efd8ac1736b84ae7e6b30329
1siikaa <srishtidwivedi672@gmail.com>
2022-11-28 16:59:41 +0530

    done

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -1,5 +1,10 @@
 const userModel = require("../Models/userModel")
 const { isValidName, isValidEmail, isValidNumber, isValidPassword,capitilize} = require("../validator/validator")
+const jwt = require('jsonwebtoken')
+
+//------------------------------------------------------------------- Post Api for User Creation ----------------------------------------------------
+
+
 
 const createdUser = async function (req, res) {
     try {
@@ -34,4 +39,50 @@ const createdUser = async function (req, res) {
     }
 }
 
+
+//------------------------------------------------------------------- Sign In ----------------------------------------------------
+
+
+const userLogin= async function(req,res){
+    try{
+    let credentials=req.body
+    let {email,password}=credentials
+    if(Object.keys(credentials)==0){
+       return res.status(400).send({status:false, message:"email and password are required"})}
+    if(email.length==0|| password.length==0){
+       return res.status(400).send({status:false, message:"both fields are required."})
+    }
+
+if(!isValidEmail(email)){
+return res.status(400).send({status:false, message:"invalid email"})}
+
+
+if(!isValidPassword(password)){
+return res.status(400).send({status:false, message:"invalid password"})}
+
+
+let userDetail=await userModel.findOne({$or:[{email:email,password:password}]})
+if(!userDetail){
+return res.status(404).send({status:false, message:"The email or password is wrong or this user is not present at that time."})}
+
+
+const iat=Date.now()
+const exp= Date.now()
+
+let token=jwt.sign({
+email:userDetail.email,
+password:userDetail.password,
+id:userDetail._id,
+iat:iat,
+exp:exp
+},"project/booksManagementGroup22")
+
+res.setHeader("x-api-key", token)
+return res.status(200).send({status:true, message:"Success" ,data:token})}
+catch(error){
+return res.status(500).send({status:false, message: error.message})
+}}
+
+
+module.exports.userLogin= userLogin
 module.exports.createdUser = createdUser
--- src/route/route.js
+++ src/route/route.js
@@ -2,6 +2,12 @@ const express=require("express")
 const router=express.Router()
 const userController=require("../Controller/userController")
 
+//------------------------------------------------------------------- Post Api ----------------------------------------------------
+
+
 router.post("/register",userController.createdUser)
+router.post('/login', userController.userLogin)
+
+
 
 module.exports = router
